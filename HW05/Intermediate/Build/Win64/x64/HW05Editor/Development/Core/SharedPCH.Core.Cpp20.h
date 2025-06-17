612c988ab98f822a8c96120ce74b9d7e4d9c5d7c
chaenning <codmsrjf@naver.com>
2025-06-17 14:43:50 +0900

    세팅 수정

--- HW05/Intermediate/Build/Win64/x64/HW05Editor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "D:/Unreal Projects/HW05/Intermediate/Build/Win64/x64/HW05Editor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- HW05/Intermediate/Build/Win64/x64/HW05Editor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "D:/Unreal Projects/HW05/Intermediate/Build/Win64/x64/HW05Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
